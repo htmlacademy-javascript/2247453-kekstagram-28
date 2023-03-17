4ca2373c9a9300ec21494ae403c0c87fada304aa
hikkaneet <v.y.tolokonnikov@yandex.ru>
2023-03-18 01:25:57 +0400

    Завершает предварительный код вывода демо данных

--- js/thumbnails.js
+++ js/thumbnails.js
@@ -1,6 +1,6 @@
 import {createAllPhotos} from './data.js';
 
-const thumbnailsList = document.querySelector('.pictures  container');
+const thumbnailsList = document.querySelector('.pictures');
 const thumbnailsTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
 const getThumbnails = createAllPhotos();
@@ -10,7 +10,7 @@ const thumbnailsListFragment = document.createDocumentFragment();
 getThumbnails.forEach(({url, comments, likes}) => {
   const thumbnailsElement = thumbnailsTemplate.cloneNode(true);
   thumbnailsElement.querySelector('.picture__img').src = url;
-  thumbnailsElement.querySelector('.picture__comments').textContent = comments;
+  thumbnailsElement.querySelector('.picture__comments').textContent = comments.length;
   thumbnailsElement.querySelector('.picture__likes').textContent = likes;
   thumbnailsListFragment.appendChild(thumbnailsElement);
 });
