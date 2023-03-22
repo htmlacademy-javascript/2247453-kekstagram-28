085cb1510a6e1932a9d56102b26b971a03ab1312
hikkaneet <v.y.tolokonnikov@yandex.ru>
2023-03-23 01:52:15 +0400

    Добавляет отображение большого изображения

--- js/big-picture-popup.js
+++ js/big-picture-popup.js
@@ -1,10 +1,16 @@
 import { isEscapeKey, isEnterKey } from './util.js';
+import { getCurrentItemByDatasetId, renderBigPictureContent, showComments } from './big-picture-render.js';
 
 const thumbnailsContainer = document.querySelector('.pictures');
 const bigPicturePopup = document.querySelector('.big-picture');
 const bigPictureCloseButton = document.querySelector('.big-picture__cancel');
 const commentCount = document.querySelector('.social__comment-count');
 const commentsLoader = document.querySelector('.comments-loader');
+const bigImage = document.querySelector('.big-picture__img img');
+const likesCount = document.querySelector('.likes-count');
+const allCommentCount = document.querySelector('.comments-count');
+const socialCaption = document.querySelector('.social__caption');
+const socialComments = document.querySelector('.social__comments');
 
 const onPopupEscKeydown = (evt) => {
   if (isEscapeKey(evt)) {
@@ -14,7 +20,7 @@ const onPopupEscKeydown = (evt) => {
 };
 
 //Открытие всплывающего окна
-function openBigPicture () {
+function openBigPicture (data) {
   thumbnailsContainer.addEventListener('click', (evt) => {
     if (evt.target.matches('.picture__img')) {
       evt.preventDefault();
@@ -26,6 +32,11 @@ function openBigPicture () {
       commentsLoader.classList.add('hidden'); //Скрывает блок загрузки новых комментариев
 
       document.body.classList.add('modal-open'); //Блокировка скрола на body
+
+      const currentThumbnail = evt.target.closest('[data-thumbnail-id]'); //Получение значения ID из dataset
+      const currentPicture = getCurrentItemByDatasetId(data, currentThumbnail); //Запись в переменную элемент с нужным ID
+      renderBigPictureContent(currentPicture,bigImage,likesCount,allCommentCount,socialCaption); //Заполнение HTML значениями из объекта
+      showComments(socialComments, currentPicture.comments); //Отрисовка комментариев
     }
   });
 
@@ -39,6 +50,11 @@ function openBigPicture () {
       commentsLoader.classList.add('hidden');
 
       document.body.classList.add('modal-open');
+
+      const currentThumbnail = evt.target.closest('[data-thumbnail-id]');
+      const currentPicture = getCurrentItemByDatasetId(data, currentThumbnail);
+      renderBigPictureContent(currentPicture,bigImage,likesCount,allCommentCount,socialCaption);
+      showComments(socialComments, currentPicture.comments);
     }
   });
 }
--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 import { createAllPhotos } from './data.js';
 import { getDemoThumbnails } from './thumbnails.js';
 import { openBigPicture,closeBigPicture } from './big-picture-popup.js';
-import './big-picture-render.js';
 
-getDemoThumbnails(createAllPhotos());
-openBigPicture();
+const demoData = createAllPhotos();
+getDemoThumbnails(demoData);
+openBigPicture(demoData);
 closeBigPicture();
