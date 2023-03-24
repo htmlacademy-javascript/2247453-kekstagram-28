8474c2dab96a0d2fa13896de928d549feafe323e
Keks <keks@htmlacademy.ru>
2023-03-24 06:53:06 +0300

    :heavy_check_mark: Сборка #7

--- 7/js/main.js
+++ 7/js/main.js
@@ -1,8 +1,8 @@
 import { createAllPhotos } from './data.js';
 import { getDemoThumbnails } from './thumbnails.js';
-import { openBigPicture,closeBigPicture } from './big-picture-popup.js';
+import { addOpenPictureHandler,addClosePictureHandler } from './big-picture-popup.js';
 
 const demoData = createAllPhotos();
 getDemoThumbnails(demoData);
-openBigPicture(demoData);
-closeBigPicture();
+addOpenPictureHandler(demoData);
+addClosePictureHandler();
